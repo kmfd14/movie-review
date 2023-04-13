fee066d4fd72e84ca06f7f120eda9f1bbc343bd6
kmfd14 <kmfd14@gmail.com>
2023-04-13 16:09:05 +0800

    added address region model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_055355) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_080336) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
